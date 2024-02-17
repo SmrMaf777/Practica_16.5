a0574867faffcac02e35f8ee6eb2a6420ceef4ef
Alexey Krickiy <kritskiyav@yandex.ru>
2024-02-17 18:22:17 +0400

    first commit

--- Practica_16.5/ViewController.swift
+++ Practica_16.5/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello Swift"
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setupViews()
+        setupConstraints()
     }
-
-
+    
+    private func setupViews() {
+        view.addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
